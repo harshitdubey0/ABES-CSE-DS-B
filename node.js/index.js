5bceff699ca98a570c11713b12dc5a30d48091ef
harshitdubey0 <harshitdubey11578@gmail.com>
2026-02-03 11:55:56 +0530

    created 2 file

--- node.js/index.js
+++ node.js/index.js
@@ -5,15 +5,40 @@ const fs = require('fs');
 
 //  fs.writeFile("./ds-b.txt", "we are code " ,()=>{});
 
- //fs.writeFileSync("./abes.txt", "Hello Harshit from ABES COLLEGE OF ENGINEERING");
 
-// Function to read a file asynchronously
-fs.readFile("./abes.txt","utf-8",(err,result)=>{
+// write using asynchronous method
+//  fs.writeFile("./A1.txt", "we are code " ,()=>{});
+// fs.writeFile("./A2.txt", "we are code " ,()=>{});
+
+
+
+// read using asynchronous method
+fs.readFile("./A1.txt","utf-8",(err,result)=>{
     if(err){
         console.log("Error", err);
     }
     else{
         console.log(result);
     }
- });
+});
+fs.readFile("./A2.txt","utf-8",(err,result)=>{
+    if(err){
+        console.log("Error", err);
+    }
+    else{
+        console.log(result);
+    }
+});
+
+ //fs.writeFileSync("./abes.txt", "Hello Harshit from ABES COLLEGE OF ENGINEERING");
+
+// Function to read a file asynchronously
+// fs.readFile("./abes.txt","utf-8",(err,result)=>{
+//     if(err){
+//         console.log("Error", err);
+//     }
+//     else{
+//         console.log(result);
+//     }
+//  });
  
